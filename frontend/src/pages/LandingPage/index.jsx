15f1cc535789894bdbaf7698f6459dccefe0e888
Abdullah Nadeem <abdullahnadeemdev1@gmail.com>
2026-04-17 17:32:44 +0500

    created features folder

--- frontend/src/pages/LandingPage/index.jsx
+++ frontend/src/pages/LandingPage/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../../components/layout/Navbar/Navbar";
 import Hero from "../../components/landing/Hero";
+import Features from "../../components/landing/Features";
 
 const LandingPage = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Features />
     </div>
   );
 };
