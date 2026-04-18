7927e1a5e4c818baeefb207e21f421b0cef7a036
Abdullah Nadeem <abdullahnadeemdev1@gmail.com>
2026-04-18 10:43:25 +0500

    added custom button and input field

--- frontend/src/components/shared/InputField.jsx
+++ frontend/src/components/shared/InputField.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
-const InputField = () => {
-  return <div>InputField</div>;
+const InputField = ({ icon: Icon, lavel, name, ...props }) => {
+  return (
+    <div>
+      <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+        {label}
+      </label>
+      <div className="relative">
+        {Icon && (
+          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none  ">
+            <Icon className="w-4 h-4 text-gray-400" />
+          </div>
+        )}
+        <input
+          type="text"
+          id={name}
+          name={name}
+          {...props}
+          className={`w-full h-11 py-2 border border-gray-200 runded-xl bg-white text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:border-transparent  transition-all duration-200 ${Icon ? "pl-10" : ""}`}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default InputField;
--- frontend/src/pages/LoginPage/index.jsx
+++ frontend/src/pages/LoginPage/index.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { useState } from "react";
+import { Link, userNavigate } from "react-router";
+import { Mail, Lock, BookOpen } from "lucide-react";
+import toast from "react-toastify";
 
-const index = () => {
+import InputField from "../../components/shared/InputField";
+import Button from "../../components/shared/Button";
+import { useAuth } from "../../context/AuthContext";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+
+const Login = () => {
   return <div>login</div>;
 };
 
-export default index;
+export default Login;
